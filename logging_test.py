fb3c72066225dcbe4f6bdefac9e451066f7cc029
marcus luetolf <marcus.luetolf@bluewin.ch>
2022-01-27 11:50:51 +0100

    Update logging_test.py

--- logging_test.py
+++ logging_test.py
@@ -3,7 +3,9 @@
 from time import sleep
 import logging
 
-logging.basicConfig(filename='/home/pi/mylog1.log', filemode='a', level=logging.INFO)
+logging.basicConfig(filename='/home/pi/mylog1.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+ 13                      level=logging.INFO)
+
 n = 0
 while n < 7:
     print('printing')
