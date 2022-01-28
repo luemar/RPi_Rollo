6b7aa62b416d4553384fd895b35c75e3f9964247
marcus luetolf <marcus.luetolf@bluewin.ch>
2022-01-28 12:13:29 +0100

    Update rollo_man.py

--- rollo_man.py
+++ rollo_man.py
@@ -3,123 +3,161 @@ from gpiozero import Button, LED
 from signal import pause
 import time
 import logging
+import schedule
+import threading
 
-logging.basicConfig(filename='/home/pi/mylog1.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
- 13                      level=logging.INFO)
+logging.basicConfig(filename='/home/pi/mylog1.log', filemode='a',\
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',\
+                    level=logging.INFO)
 
+Taste_li_autoauf = Button(17, hold_time = 1)
+Taste_li_autoab = Button(22, hold_time = 1)
+Taste_li_manauf = Button(27)
+Taste_li_manab = Button(5)
+Taste_re_autoauf = Button(6, hold_time = 1)
+Taste_re_autoab = Button(19, hold_time = 1)
+Taste_re_manauf = Button(13)
+Taste_re_manab = Button(26)
 
-btn1 = Button(17, hold_time = 1)
-btn2 = Button(22, hold_time = 1)
-btn3 = Button(27)
-btn4 = Button(5)
-btn5 = Button(6, hold_time = 1)
-btn6 = Button(19, hold_time = 1)
-btn7 = Button(13)
-btn8 = Button(26)
-
-led1 = LED(18)
+led1 = LED(25)
 led2 = LED(23)
 led3 = LED(24)
-led4 = LED(25)
+led4 = LED(18)
 
 Button.was_held = False
 
-def held_li_up(btn1):
-    print("button li up held")
+def held_li_up(Taste_li_autoauf):
+    print("Taste li auf gehalten")
     Button.was_held = True
     led1.on()
     time.sleep(18)
     led1.off()
 
-def released_li_up(btn1):
-    if not btn1.was_held:
+def released_li_up(Taste_li_autoauf):
+    if not Taste_li_autoauf.was_held:
         Button.was_held = False
 
-btn1.when_held = held_li_up
-btn1.when_released = released_li_up
-
-def held_li_down(btn2):
-    print("button li down held")
+def held_li_down(Taste_li_autoab):
+    print("Taste li ab gehalten")
     Button.was_held = True
     led2.on()
     time.sleep(18)
     led2.off()
 
-def released_li_down(btn2):
-    if not btn1.was_held:
+def released_li_down(Taste_li_autoab):
+    if not Taste_li_autoab.was_held:
         Button.was_held = False
 
-btn2.when_held = held_li_down
-btn2.when_released = released_li_down
-
-def held_re_up(btn5):
-    print("button re up held")
+def held_re_up(Taste_re_autoauf):
+    print("Taste re auf gehalten")
     Button.was_held = True
     led3.on()
     time.sleep(18)
     led3.off()
 
-def released_re_up(btn5):
-    if not btn5.was_held:
-        Button.was_held = False
+def released_re_up(Taste_re_autoauf):
+    if not Taste_re_autoauf.was_held:
+       Button.was_held = False
 
-btn5.when_held = held_re_up
-btn5.when_released = released_re_up
+def held_re_down(Taste_re_autoab):
+   print("Taste re ab gehalten")
+   Button.was_held = True
+   led4.on()
+   time.sleep(18)
+   led4.off()
 
-def held_re_down(btn6):
-    print("button re down held")
-    Button.was_held = True
-    led4.on()
-    time.sleep(18)
-    led4.off()
-def released_re_down(btn6):
-    if not btn6.was_held:
-        Button.was_held = False
-
-btn6.when_held = held_re_down
-btn6.when_released = released_re_down
+def released_re_down(Taste_re_autoab):
+    if not Taste_re_autoab.was_held:
+       Button.was_held = False
 
 def on_up_li():
-    print("button li up pressed")
+    print("Taste li auf gedrückt")
     led1.on()
 
 def off_up_li():
-    print("button li up released")
+    print("Taste li auf losgelassen")
     led1.off()
 
 def on_down_li():
-    print("button li down pressed")
-    led2.on()
+   print("Taste li ab gedrückt")
+   led2.on()
 
 def off_down_li():
-    print("button li down released")
-    led2.off()
-
-btn3.when_pressed = on_up_li
-btn3.when_released = off_up_li
-btn4.when_pressed = on_down_li
-btn4.when_released = off_down_li
+   print("Taste li ab losgelassen")
+   led2.off()
 
 def on_up_re():
-    print("button re up pressed")
-    led3.on()
+   print("Taste re auf gedrückt")
+   led3.on()
 
 def off_up_re():
-    print("button re up released")
-    led3.off()
+   print("Taste re auf losgelassen")
+   led3.off()
 
 def on_down_re():
-    print("button re down pressed")
-    led4.on()
+   print("Taste re ab gedrückt")
+   led4.on()
 
 def off_down_re():
-    print("button re down released")
+   print("Taste re ab losgelassen")
+   led4.off()
+
+def zeitschalt_hochfahren_an():
+    led1.on()
+    time.sleep(18)
+    led1.off()
+    logging.info('Rollo li offen')
+    time.sleep(2)
+
+    led3.on()
+    time.sleep(18)
+    led3.off()
+    logging.info('Rollo re offen')
+
+def zeitschalt_runterfahren_an():
+    led2.on()
+    time.sleep(18)
+    led2.off()
+    logging.info('Rollo li geschlossen')
+
+    led4.on()
+    time.sleep(18)
     led4.off()
+    logging.info('Rollo re geschlossen')
 
-btn7.when_pressed = on_up_re
-btn7.when_released = off_up_re
-btn8.when_pressed = on_down_re
-btn8.when_released = off_down_re
+def schedule_thread():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-print("Init abgeschlossen")
-pause()
+def main():
+    schedule.every().day.at("11:31").do(zeitschalt_hochfahren_an)
+    schedule.every().day.at("11:32").do(zeitschalt_runterfahren_an)
+
+    threading.Thread(target=schedule_thread).start()
+
+    Taste_li_autoauf.when_held = held_li_up
+    Taste_li_autoauf.when_released = released_li_up
+    Taste_li_autoab.when_held = held_li_down
+    Taste_li_autoab.when_released = released_li_down
+
+    Taste_re_autoauf.when_held = held_re_up
+    Taste_re_autoauf.when_released = released_re_up
+    Taste_re_autoab.when_held = held_re_down
+    Taste_re_autoab.when_released = released_re_down
+
+    Taste_li_manauf.when_pressed = on_up_li
+    Taste_li_manauf.when_released = off_up_li
+    Taste_li_manab.when_pressed = on_down_li
+    Taste_li_manab.when_released = off_down_li
+
+    Taste_re_manauf.when_pressed = on_up_re
+    Taste_re_manauf.when_released = off_up_re
+    Taste_re_manab.when_pressed = on_down_re
+    Taste_re_manab.when_released = off_down_re
+
+    pause()
+    print("Init abgeschlossen")
+
+if __name__ == "__main__":
+    main()
